6c8a0e51a11108d1efc7f32d95aaa6cba7310c08
Aaron S <grayjackel5@gmail.com>
2023-10-28 12:02:19 -0700

    commands finished

--- A2Prj/src/com/mycompany/a2/GameCommands/CommandCollideWithFoodStation.java
+++ A2Prj/src/com/mycompany/a2/GameCommands/CommandCollideWithFoodStation.java
@@ -1,7 +1,22 @@
 package com.mycompany.a2.GameCommands;
 
 import com.codename1.ui.Command;
+import com.codename1.ui.events.ActionEvent;
+import com.mycompany.a2.GameWorld;
 
 public class CommandCollideWithFoodStation extends Command {
+    private static String commandName = "Command_Collide_With_Food_Station";
+
+    private GameWorld gameWorld;
+
+    public CommandCollideWithFoodStation(GameWorld newGameWorld) {
+        super(commandName);
+        this.gameWorld = newGameWorld;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        gameWorld.collideFoodStation();
+    }
 
 }
--- A2Prj/src/com/mycompany/a2/GameCommands/CommandCollideWithSpider.java
+++ A2Prj/src/com/mycompany/a2/GameCommands/CommandCollideWithSpider.java
@@ -1,7 +1,21 @@
 package com.mycompany.a2.GameCommands;
 
 import com.codename1.ui.Command;
+import com.codename1.ui.events.ActionEvent;
+import com.mycompany.a2.GameWorld;
 
 public class CommandCollideWithSpider extends Command {
+    private static String commandName = "Command_Collide_With_Spider";
 
+    private GameWorld gameWorld;
+
+    public CommandCollideWithSpider(GameWorld newGameWorld) {
+        super(commandName);
+        this.gameWorld = newGameWorld;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        gameWorld.collideSpider();
+    }
 }
